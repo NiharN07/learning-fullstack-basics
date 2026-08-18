dfa08a22d0b1b74a053e5c4fbcf5b0f3ea3f499c
niharn07 <nihar752007@gmail.com>
2026-08-18 17:27:51 +0530

    game completed

--- learning js/index.js
+++ learning js/index.js
@@ -115,9 +115,9 @@ function update(location) {
     button1.innerText = location["button text"][0];
 	button2.innerText = location["button text"][1];
 	button3.innerText = location["button text"][2];
-    button1.onclick = location["button function"][0];
-	button2.onclick = location["button function"][1];
-	button3.onclick = location["button fynction"][2];
+    button1.onclick = location["button functions"][0];
+	button2.onclick = location["button functions"][1];
+	button3.onclick = location["button functions"][2];
 	text.innerText = location.text;
 }
 
@@ -192,8 +192,8 @@ function fightDragon() {
 }
 
 function goFight() {
-	update(llocations[3]);
-	monsterHealth = monster[fighting].health;
+	update(locations[3]);
+	monsterHealth = monsters[fighting].health;
 	monsterStats.style.display = "block"; 
 	monsterNameText.innerText = monsters[fighting].name;
 	monsterHealthText.innerText = monsterHealth;
@@ -202,14 +202,13 @@ function goFight() {
 function attack() {
 	text.innerText = "The" + monsters[fighting].name + "attacks.";
 	text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-   
+    health -= getMonsterAttackValue(monsters[fighting].level);
 	if (isMonsterHit()) {
-	    health -= getMonsterAttackValue(monster[fighting].level);
+	    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
 	} else {
 		text.innerText += "You miss.";
 	}
 	
-	monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
 	healthText.innerText = health;
 	monsterHealthText.innerText = monsterHealth;
 	if (health <= 0) {
@@ -217,13 +216,13 @@ function attack() {
 	} else if (monsterHealth <= 0) {
 	    fighting === 2 ? winGame() : defeatMonster();
 	}
-}
+
 
     if (Math.random()<= .1 && inventory.length1== 1) {
         text.innerText += "Your" + inventory.pop() + "breaks.";
 		currentWeapon--;
 	}
-
+}
 function getMonsterAttackValue(level) {
 	let hit = (level * 5) - (Math.floor(Math.random() * xp));
 	console.log(hit);
@@ -243,8 +242,7 @@ function defeatMonster() {
 	xp += monsters[fighting].level;
 	goldText.innerText = gold;
 	xpText.innerText = xp;
-	update(location[4]);
-
+	update(locations[4]);
 }
 
 function lose() {
@@ -288,7 +286,7 @@ function pick(guess) {
 	text.innerText = "You picked" + guess + ".Here are the random numbers:\n";
 
 	for (let i = 0; i < 10; i++) {
-	    text.innerText += number[i] + "\n";	
+	    text.innerText += numbers[i] + "\n";	
 	} 
 
 	if (numbers.indexOf(guess) !== -1) {
